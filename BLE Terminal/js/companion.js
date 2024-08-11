1670bdd8a910b888a39a788d0d8feb269fc94743
OpenDev Club <opendevclub@gmail.com>
2024-08-11 21:12:53 +0530

    Add files via upload

---
+++ BLE Terminal/js/companion.js
@@ -0,0 +1,9 @@
+
+(function() {
+  'use strict';
+  var workerScript = document.currentScript && document.currentScript.dataset.serviceWorker;
+
+  if (workerScript && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.register(workerScript);
+  }
+})();
